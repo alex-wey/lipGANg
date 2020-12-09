37f4c075e951b189200a46517372f6992ef73a06
Ethan <echung2018@pingry.org>
2020-12-09 01:01:06 -0500

    readding the preprocess helper functions and files used for preprocessing lrs3

--- filelists/get_names.py
+++ filelists/get_names.py
@@ -4,7 +4,9 @@ def win():
     # loop through all of trainvals folders and get the folder names
     # get the file names too
     final = set()
-    l = glob.glob("/Users/ethanchung/Programming/cs147/final_project/LipGAN-fully_pythonic/trainval/*")
+    final_list = []
+    # TODO: note, we moved trainval to the lipGANg!!!
+    l = glob.glob("/Users/ethanchung/Programming/cs147/final_project/LipGAN-fully_pythonic/lipGANg/trainval/*")
     for folder in l:
         x = glob.glob(folder + "/*")
         for items in x:
@@ -13,9 +15,12 @@ def win():
             list_x[-1] = list_x[-1][:-4]
             joined = list_x[-2] + "/" + list_x[-1] + "\n"
             #print(joined)
-            final.add(joined)
+            if joined not in final:
+                final.add(joined)
+                final_list.append(joined)
+
     
-    L = list(final)
+    L = final_list
     
     # write to file
     file1 = open('filelists/train_val.txt', 'w')
